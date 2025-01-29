732c46fc90fe2ae128b44f4220a080a01a37c291
imtester122 <assemsd8@gmail.com>
2025-01-29 10:22:57 -0500

    work.js

--- work.js
+++ work.js
@@ -9,3 +9,10 @@ image.addEventListener('mouseout' ,()=>{
 
     image.src ='tch.png';
 })
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-94GYTTBD9Z');
