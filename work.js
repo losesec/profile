b717266e35764cc887682bf9368d9b973340702f
Lose <losesec1@gmail.com>
2025-02-28 19:08:36 -0500

    work.js

--- work.js
+++ work.js
@@ -9,10 +9,3 @@ image.addEventListener('mouseout' ,()=>{
 
     image.src ='tch.png';
 })
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-1ED5SPKCND');
