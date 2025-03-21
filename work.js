3ff0c3a107f0d6b26d038d8f4064aa5a18a086ea
lose_sec <losesec1@gmail.com>
2025-03-21 13:23:50 -0400

    work.js

--- work.js
+++ work.js
@@ -9,3 +9,10 @@ image.addEventListener('mouseout' ,()=>{
 
     image.src ='tch.png';
 })
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P2FPZSBD73')
